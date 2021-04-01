ca15d262c6f36daf53b5a4a31eddfe9955a99e98
MUHAMMAD REZA UTAMA PULUNGAN <rezapulungan@student.ub.ac.id>
2021-04-01 20:30:08 +0700

    first commit

---
+++ app/src/main/java/piuwcreative/mycontactlist/ContactsList.java
@@ -0,0 +1,42 @@
+package piuwcreative.mycontactlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
+
+public class ContactsList extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ArrayList<Kontak> kontaks;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts_list);
+
+        ArrayList<String> names = getIntent().getStringArrayListExtra("names");
+        ArrayList<String> phones = getIntent().getStringArrayListExtra("phones");
+
+        kontaks = new ArrayList<>();
+        for (int i = 0; i < names.size(); i++) {
+            Kontak kontak = new Kontak();
+            kontak.setName(names.get(i));
+            kontak.setPhone(phones.get(i));
+            kontaks.add(kontak);
+        }
+
+        recyclerView = findViewById(R.id.rv_kontaks);
+
+        recyclerView.setHasFixedSize(true);
+        LinearLayoutManager ll = new LinearLayoutManager(this);
+        ll.setOrientation(RecyclerView.VERTICAL);
+        recyclerView.setLayoutManager(ll);
+        KontakAdapter adapter = new KontakAdapter(this);
+        adapter.setKontaks(kontaks);
+        recyclerView.setAdapter(adapter);
+
+    }
+}
